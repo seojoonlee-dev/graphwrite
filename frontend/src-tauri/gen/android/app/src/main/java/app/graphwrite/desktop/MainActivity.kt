816a1c5344525b6d37bfd663d89fc15a1f574c3a
flowerboi1 <developer.seojoonlee@gmail.com>
2026-06-14 21:19:58 +0900

    Added android build support

---
+++ frontend/src-tauri/gen/android/app/src/main/java/app/graphwrite/desktop/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.graphwrite.desktop
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
